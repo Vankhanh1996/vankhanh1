10026fe687828de23f72de93f51d5aaeef5bae9e
CLOUDFLY <124904465+Vankhanh1996@users.noreply.github.com>
2023-02-20 19:02:10 +0700

    Update 80443fix.sh

--- 80443fix.sh
+++ 80443fix.sh
@@ -51,7 +51,7 @@ install() {
 install_XrayR() {
 	[[ -z $(type -P curl) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} curl
 	[[ -z $(type -P socat) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} socat
-	bash <(curl -Ls https://raw.githubusercontent.com/longyi8/XrayR/master/install.sh)
+	bash <(curl -Ls https://raw.githubusercontent.com/Vankhanh1996/XrayRCute/master/install.sh)
 }
 
 makeConfig() {
